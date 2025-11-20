361bd98b14dffdfe5a195cd84983dde659a0b77b
Daniela Garcia <danielagarcia9@gmail.com>
2025-11-20 11:58:49 -0500

    Changes to template_desplegables.py

--- src/data/template_desplegables.py
+++ src/data/template_desplegables.py
@@ -1,24 +1,27 @@
 template = {
     "gerencias": [
         "Gerencia General",
-        "Gerencia Ventas",
-        "Gerencia Operaciones",
-        "Gerencia Recursos Humanos",
-        "Gerencia Finanzas",
-        "Gerencia TI"
+        "Ventas",
+        "Operaciones",
+        "Recursos Humanos",
+        "Finanzas",
+        "TI"
     ],
     "subgerencias": [
         "Oficina Ejecutiva",
         "Ventas Nacionales", 
         "Ventas Internacionales",
         "Producción", 
+        "Mantenimiento",
         "Logística",
         "Reclutamiento", 
         "Capacitación",
         "Contabilidad", 
         "Presupuesto",
         "Infraestructura", 
-        "Desarrollo de Software"
+        "Desarrollo de Software",        
+        "Superindendencia Madera",
+        "Superindendencia Mantención"
     ],
     "areas": [
         "Estrategia", 
@@ -42,7 +45,9 @@ template = {
         "Red", 
         "Hardware",
         "Desarrollo Web", 
-        "Desarrollo de Apps"
+        "Desarrollo de Apps",
+        "Preparación Madera",
+        "Área Maderas"
     ],
     "desafios": [
         "Crecer al doble en 10 años",
@@ -50,7 +55,16 @@ template = {
         "Mejorar la eficiencia operativa",
         "Aumentar la satisfacción del cliente",
         "Reducir costos",
-        "Mejorar la comunicación interna"
+        "Mejorar la comunicación interna",
+        "Asegurar la dipsoniblidad y confiabilidad de los equipos, Liderar al equipo de mantenimiento, Supervisar  coordinar la planificación, ejecución y cierre de trabajos, Liderar Análisis de Causa Raíz, Diseñar y controlar el presupuesto por área, Gestionar la relación operativa entre mantenimiento y producción, Impulsar la implementación de la estrategia de confiabilidad, Gestionar activamente los riesgos del proceso, Implementar y verificar controles críticos, Promover una cultura de seguridad.",
+        "Asegurar la continuidad operativa, Planificar y coordinar la ejecución de tareas de mantenimiento, Liderar al equipo técnico, Gestionar repuestos y recursos críticos, Controlar y analizar indicadores de gestión, Coordinar la relación téncia-operativa, participar activamente en la implementación de la estrategia de confiabilidad.",
+        "Liderar la operación diaria del área, Garantizar condiciones seguras, Desarrollar competencias del equipo operativo, Corrdinar la planificación de mantenimiento, Monitorear lazos de control automático, Liderar análisis de cuasa raíz, Apoyar e implementar iniciativas de mejora continua y propuestas de optiización productiva y energética.",
+        "Monitorear el proceso de producción, Realizar tareas de limpieza a equipos y áreas de trabajo, coordinar con el operador de sala ajustes, Comunicarse efectivamente con operadores, Levantar desviaciones, contribuir al aprendizaje.",
+        "Monitorear variables críticas, realizar ajuste de parámetros de proceso, Realizar seguimiento y monitoreo, Tomar decisiones informadas, Comunicarse efectivamente con los operadores, Apoyar al operador de terreno, levantar desviaciones, participar en los preparativos requeridos, contribuir al aprendizaje continuo, Contribuir a la Mejora continua y  al aprendizaje, realizar tareas de limpieza de equipos y área de trabajo.",
+        "Monitorear variables críticas, realizar ajuste de parámetros de proceso, Realizar seguimiento y monitoreo, Tomar decisiones informadas, Comunicarse efectivamente con los operadores, Apoyar al operador de terreno, levantar desviaciones, participar en los preparativos requeridos, contribuir al aprendizaje continuo, dirigir la reunión de inicio de turno, monitorear la asistencia del equipo, actuar como punto de contacto princial entre operadores.",
+        "Realizar el mantenimiento y diagnóstico de fallas de los equipos, Realizar las tareas de mantenimiento preventivo, Inspeccionar regularmente las líneas de producción, Desarrollar proyectos de mejora, Diagnosticar fallas, Corregir y dar segumiento a las fallas, Cumplir con los lineamientos de seguridad, calidad y medioambiente, Validar ejecución técnica, Mantener el área en óptimas condiciones, Realizar el seguimiento de los equipos, Ejecutar procedicmientso de seguridad, Velar por la seguridad de los trabajadores, Participar en los procesos de investigación de incidentes, Corrdinar ysupervisar las actividades diarias de mantención, Apoyar a planificador y programador SAP.",
+        "Realizar mantenimiento y diagnóstico de fallas, Realizar tareas de mantenimiento, Inspeccionar regularmente las líneas de producción, Realizar levantamientos mejora continua, Cumplir programa de mantenimiento, Corregir y dar segumiento a fallas, Validar ejecución técnica, Utilizar herramientas prventivas, Ejecutar procedimientos de seguridad, Mantener la disponibilidad de los equipos, Realizar urtas de inspección, Cumplir con los linamientos de seguridad, Generar avisos en SAP",    
+        "Resgardar la continuidad operacional, el cumplimiento de las normativas legales, medioambientales y de seguridad, priorizando la Seguridad y Salud Ocupacional de la personas, el cuidado del medio ambiente y la mejora continua del proceso."
     ],
     "audiencias": [
         "Gerente",
@@ -62,7 +76,15 @@ template = {
         "Analistas",
         "Asistentes",
         "Equipo de Proyecto",
-        "Todos los equipos"
+        "Todos los equipos",
+        "Ingeniero de operaciones / Supervisor",
+        "Jefe(a)  Mecánico(a) / Supervisor",
+        "Mantenedor Mecánico",
+        "Mantenedor Mecánico Líder",
+        "Operador(a) de Sala",
+        "Operador(a) de Sala Líder",
+        "Operador(a) de Terreno",
+        "Superintendente de Mantenimiento / Supervisor"
     ],
     "modalidades": [
         "Presencial",
@@ -80,6 +102,7 @@ template = {
     ],
     "origin": [
         "DNC",
-        "SGD"
+        "SGD",
+        "Carga Excel"
     ]
 }
